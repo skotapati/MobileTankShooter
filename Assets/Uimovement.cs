2f5317089fba8ed7621d4328541f89cfca94024c
Jaskaran Bakshi <jas10022jb@gmail.com>
2016-03-28 18:41:50 -0700

    AI Tank
    got the ai tank following the player

---
+++ Assets/Uimovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uimovement : MonoBehaviour {
+	public GameObject player;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void MoveForword(){
+		transform.position += transform.forward*speed*Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.LookAt (player.transform.position);
+		MoveForword ();
+
+	}
+}
